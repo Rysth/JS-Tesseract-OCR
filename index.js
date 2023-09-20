8fc718d1452049af69e60f9b241741a4e3921955
Rysth <johnpalacios.t@gmail.com>
2023-09-20 15:44:39 -0500

    Added Face API to handle the faces.

--- index.js
+++ index.js
@@ -19,6 +19,12 @@ async function recognizeText() {
     await worker.loadLanguage(selectedLang);
     await worker.initialize(selectedLang);
 
+    // Load FaceAPI.js models
+    await faceapi.nets.tinyFaceDetector.loadFromUri('./models');
+    await faceapi.nets.faceLandmark68Net.loadFromUri('/models');
+    await faceapi.nets.faceRecognitionNet.loadFromUri('/models');
+    await faceapi.nets.ssdMobilenetv1.load('/models');
+
     // Handle image selection
     fileInput.addEventListener('change', async (e) => {
       recognizedText.textContent = 'Escaneando, espere porfavor...';
@@ -27,10 +33,63 @@ async function recognizeText() {
         const imageUrl = URL.createObjectURL(file);
         selectedImage.src = imageUrl;
 
-        // Recognize text from the selected image
+        // Create an HTML image element
+        const imgElement = document.createElement('img');
+        imgElement.src = imageUrl;
         const {
           data: { text },
         } = await worker.recognize(imageUrl);
+
+        const faceDetection = await faceapi
+          .detectSingleFace(imgElement)
+          .withFaceLandmarks()
+          .withFaceDescriptor();
+
+        if (faceDetection) {
+          const { x, y, width, height } = faceDetection.detection.box;
+
+          // Expand the size of the box to capture more of the face
+          const expandedX = Math.max(0, x - 20); // Adjust this value as needed
+          const expandedY = Math.max(0, y - 50); // Adjust this value as needed
+          const expandedWidth = width + 40; // Adjust this value as needed
+          const expandedHeight = height + 80; // Adjust this value as needed
+
+          // Create a canvas and draw the detected face on it
+          const canvas = document.createElement('canvas');
+          const ctx = canvas.getContext('2d');
+          canvas.width = expandedWidth;
+          canvas.height = expandedHeight;
+          ctx.drawImage(
+            imgElement,
+            expandedX,
+            expandedY,
+            expandedWidth,
+            expandedHeight,
+            0,
+            0,
+            expandedWidth,
+            expandedHeight,
+          );
+
+          // Convert the canvas content to an image
+          const detectedFaceImage = document.getElementById('detected-face');
+          detectedFaceImage.src = canvas.toDataURL('image/jpeg'); // You can choose the image format here
+          detectedFaceImage.style.display = 'block';
+        } else {
+          console.log('No face detected.');
+          // You can hide or clear the detected face image if needed.
+          const detectedFaceImage = document.getElementById('detected-face');
+          detectedFaceImage.src = '';
+          detectedFaceImage.style.display = 'none'; // Hide the detected face image
+        }
+
+        const dniRegex = /(?<!\d)(?:\d{9}-\d|\d{10})(?!\d)/g;
+        const dniMatch = text.match(dniRegex);
+
+        if (dniMatch && dniMatch.length > 0) {
+          const cleanDniNumber = dniMatch[0].replace('-', '');
+          console.log('DNI Number:', cleanDniNumber);
+        }
         recognizedText.textContent = text;
       }
     });
